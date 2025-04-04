83dbdcf9e8f72d133910b88f289a5c5701374656
DILEEPKS01 <ksdileep25@gmail.com>
2025-04-04 10:15:01 +0530

    Update Title.jsx

--- src/components/choose-us/Title.jsx
+++ src/components/choose-us/Title.jsx
@@ -6,10 +6,10 @@ function Title() {
     <div className="mb-10">
       <SecondaryHeading>Why choose us</SecondaryHeading>
       <TertiaryHeading color="white">
-        W can give a shape of your body here!
+        We can give a shape of your body here!
       </TertiaryHeading>
       <p className="font-medium text-gray-200">
-        At <b>CoreFit</b>, we are dedicated to helping you achieve the body of
+        At <b>BookMyFit</b>, we are dedicated to helping you achieve the body of
         your dreams. Our expert trainers and nutritionists will work with you to
         create a personalized fitness and nutrition plan that helps you reach
         your specific goals.
